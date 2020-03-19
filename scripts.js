e6a7d1a6cbf8ccbdd62fa178f47ecd392b0f317c
kumahacker <kumahavana@gmail.com>
2020-03-19 15:52:14 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,9 +1,3 @@
-function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
-
 $(function(){
 
     $('.tabs').tabs();
@@ -16,25 +10,11 @@ $(function(){
 });
 
 function formatDate(dateStr) {
-    var date = new Date(dateStr);
-    var year = date.getFullYear();
-    var month = (1 + date.getMonth()).toString().padStart(2, '0');
-    var day = date.getDate().toString().padStart(2, '0');
-    return day + '/' + month + '/' + year;
+    return moment(dateStr).format('DD/MM/YYYY');
 }
 
 function formatDateTime(dateStr) {
-    var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-    var date = new Date(dateStr);
-    var month = date.getMonth();
-    var day = pad(date.getDay(),2);
-    var hour = (date.getHours() < 12) ? date.getHours() : date.getHours() - 12;
-    var minutes = date.getMinutes();
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
-    var amOrPm = (date.getHours() < 12) ? "am" : "pm";
-    return day + ' de ' + months[month] + ' a las ' + hour + ':' + minutes + amOrPm;
+    return moment(dateStr).format('D [de] MMMM [del] YYYY [a las] h:mm A');
 }
 
 function toggleWriteModal() {
